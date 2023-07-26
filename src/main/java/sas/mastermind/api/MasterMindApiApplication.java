ffdecc9c00e8ed632f1373d169ae1796a41b418f
PacoLapLinux <paco.morando@gmail.com>
2023-07-26 14:41:36 -0600

    **Init Commit**

---
+++ src/main/java/sas/mastermind/api/MasterMindApiApplication.java
@@ -0,0 +1,13 @@
+package sas.mastermind.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterMindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterMindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sas/mastermind/api/MasterMindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sas.mastermind.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterMindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
